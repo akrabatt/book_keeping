d7788560d32a90f302dcc931e3d922607058d137
akrabatt <starwars-1999@yandex.ru>
2024-05-05 23:17:46 +0200

    added headder file

---
+++ str_book.h
@@ -0,0 +1,9 @@
+#include <string>
+
+typedef struct BOOK
+{
+    std::string title;  // пременная для названия книги
+    std::string author; // пременная для автора книги
+    int year;           // пременная для года выпуска книги
+    std::string genre;  // пременная для жанра книги
+};
