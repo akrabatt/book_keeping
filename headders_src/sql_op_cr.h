cada1a16eb6dbb7b812dbfd8a9feeac9239b2156
akrabatt <starwars-1999@yandex.ru>
2024-05-08 23:04:22 +0200

    added new file sql_op_cr

---
+++ headders_src/sql_op_cr.h
@@ -0,0 +1,9 @@
+/* этот файл служит для импорта по проекту функции по открытию или созданию базы данных */
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include "sql_src/sqlite3.h"
+
+sqlite3 *open_or_create_db();
+
+#endif /* DATABASE_H */
