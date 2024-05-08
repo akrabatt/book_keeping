d6f8a3c0c66b6b850c8fbba39c12883f00797c93
akrabatt <starwars-1999@yandex.ru>
2024-05-09 00:49:25 +0200

    save point_5 before changes

--- headders_src/extern.h
+++ headders_src/extern.h
@@ -1,19 +1,10 @@
 /* здесь будут находиться все extern функции и переменные */
-//#include "sql_src/sqlite3.h"
-//#include "headders_src/sql_op_cr.h"
-#include "sql_op_cr.h"
+
+/* заголовочные файлы */
+#include "sql_op_cr.h" // заголовочный файл с операциями создания базы данных
+
 /* переменные */
 extern int menu_ch; // переменная для хранения выбранного пункта меню
 
 /* функции */
-extern int menu();               // функция меню
-// sqlite3 open_or_create_db(); // функция открытия или создания базы данных
-
-// #ifndef DATABASE_H
-// #define DATABASE_H
-
-// #include "sqlite3.h"
-
-// sqlite3 *open_or_create_db();
-
-// #endif /* DATABASE_H */
+extern int menu(); // функция меню
