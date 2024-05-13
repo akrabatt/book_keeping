7683d74d4c058798a4a78220f8559806f455dd2c
Dmitrii_rz <starwars-1999@yandex.ru>
2024-05-13 13:31:44 +0200

    add new comments in temp.cpp

--- temp.cpp
+++ temp.cpp
@@ -18,6 +18,7 @@ void listTables(const char *databasePath, std::string &selectedTable)
     const char *query = "SELECT name FROM sqlite_master WHERE type='table';"; // SQL-запрос для получения списка таблиц
     std::cout << "Available Tables:" << std::endl;                            // Вывод заголовка списка таблиц
 
+    /* тут мы выводим список таблиц */
     rc = sqlite3_exec(db, query, [](void *data, int argc, char **argv, char **colName) -> int { // Выполнение SQL-запроса
         std::cout << "Table " << tableCount << ": " << argv[0] << std::endl;                    // Вывод названия таблицы с номером
         tableCount++;                                                                           // Увеличение счетчика таблиц
@@ -33,6 +34,7 @@ void listTables(const char *databasePath, std::string &selectedTable)
 
     sqlite3_close(db); // Закрытие базы данных SQLite
 
+    /* даем выбор пользователю */
     std::cout << "Enter the number of the table you want to select: "; // Предложение пользователю выбрать таблицу
     int choice;
     std::cin >> choice; // Чтение выбора пользователя
@@ -45,10 +47,11 @@ void listTables(const char *databasePath, std::string &selectedTable)
         return;                                                                  // Выход из функции
     }
 
-    tableCount = 1;                                                                                                   // Сброс счетчика таблиц
+    tableCount = 1; // Сброс счетчика таблиц
+    /* а вот тут мы прогоняем список таблиц заново, но не выводим его а выполняем сравнение и занесение в переменную */
     rc = sqlite3_exec(db, query, [&selectedTable, choice](void *data, int argc, char **argv, char **colName) -> int { // Выполнение SQL-запроса
-        if (tableCount == choice)
-        {                            // Если номер таблицы совпадает с выбором пользователя
+        if (tableCount == choice)                                                                                     // Если номер таблицы совпадает с выбором пользователя
+        {
             selectedTable = argv[0]; // Запись названия выбранной таблицы в переменную
             return 1;                // Прерывание выполнения после выбора таблицы
         }
@@ -77,3 +80,99 @@ int main()
 
     return 0; // Возврат успешного завершения программы
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+
+Блок 1: Объявление переменных и открытие базы данных
+
+cpp
+Copy code
+void listTables(const char *databasePath, std::string &selectedTable)
+{
+    sqlite3 *db;
+    char *errorMessage = nullptr;
+    static int tableCount = 1;
+
+    int rc = sqlite3_open(databasePath, &db);
+    if (rc != SQLITE_OK)
+    {
+        std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return;
+    }
+В этом блоке объявляются переменные db, errorMessage и tableCount, а также происходит попытка открыть базу данных SQLite по заданному пути databasePath.
+В случае неудачного открытия базы данных выводится сообщение об ошибке, и программа завершает выполнение.
+Блок 2: Получение списка таблиц
+
+cpp
+Copy code
+    const char *query = "SELECT name FROM sqlite_master WHERE type='table';";
+    std::cout << "Available Tables:" << std::endl;
+
+    rc = sqlite3_exec(db, query, [](void *data, int argc, char **argv, char **colName) -> int {
+        std::cout << "Table " << tableCount << ": " << argv[0] << std::endl;
+        tableCount++;
+        return 0;
+    },
+    nullptr, &errorMessage);
+
+    if (rc != SQLITE_OK)
+    {
+        std::cerr << "SQL error: " << errorMessage << std::endl;
+        sqlite3_free(errorMessage);
+    }
+В этом блоке формируется SQL-запрос для получения списка таблиц из базы данных.
+SQL-запрос исполняется с использованием sqlite3_exec, и для каждой таблицы выводится информация, как это показано в лямбда-выражении.
+Если исполнение SQL-запроса не удалось, выводится сообщение об ошибке.
+
+Блок 3: Выбор пользователем таблицы
+
+cpp
+Copy code
+    std::cout << "Enter the number of the table you want to select: ";
+    int choice;
+    std::cin >> choice;
+
+    rc = sqlite3_open(databasePath, &db);
+    if (rc != SQLITE_OK)
+    {
+        std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return;
+    }
+Пользователю предлагается ввести номер выбранной таблицы.
+Если базу данных необходимо открыть повторно, выполняется попытка открыть базу данных SQLite по тому же пути.
+Блок 4: Сохранение выбранной таблицы
+
+cpp
+Copy code
+    tableCount = 1;
+
+    rc = sqlite3_exec(db, query, [&selectedTable, choice](void *data, int argc, char **argv, char **colName) -> int {
+        if (tableCount == choice)
+        {
+            selectedTable = argv[0];
+            return 1;
+        }
+        tableCount++;
+        return 0;
+    },
+    nullptr, &errorMessage);
+
+    if (rc != SQLITE_OK)
+    {
+        std::cerr << "SQL error: " << errorMessage << std::endl;
+        sqlite3_free(errorMessage);
+    }
+
+    sqlite3_close(db);
+}
+Сбрасывается счетчик таблиц.
+Выполняется SQL-запрос, в котором пользователю предлагается выбрать таблицу по номеру.
+В случае успешного выбора таблицы, ее название сохраняется в переменной selectedTable.
+Если исполнение SQL-запроса не удалось, выводится сообщение об ошибке.
+База данных закрывается.
+Эти четыре блока функции listTables объясняют процесс открытия базы данных SQLite, вывода списка таблиц, выбора пользователем таблицы и сохранения выбранной таблицы в переменную.
+
+ */
