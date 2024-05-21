a3d56bbe9521fd68eec783332535962d66435408
akrabatt <starwars-1999@yandex.ru>
2024-05-21 22:33:10 +0200

    again added new comments

--- fun_sql.cpp
+++ fun_sql.cpp
@@ -407,14 +407,16 @@ void add_book_in_table(sqlite3 *db, const std::string &table_name)
     std::cout << " \nwould you like to create a new book again ?\n";
     while (true)
     {
-        std::cout << "1. Yes, create a new book\n";
-        std::cout << "2. No, return in main menu\n";
+        /* выводим предложения выбраь функцию */
+        std::cout << "1. Yes, create a new book\n";  // создать новую книгу
+        std::cout << "2. No, return in main menu\n"; // вернуться в меню
         std::cout << "..: ";
-        int ch;
-        std::cin >> ch;
+        int ch;         // переменная для выбора
+        std::cin >> ch; // предлагаем ввести переменнуую
         std::cout << "\n";
 
-        /* проверка на то что введено число, а не символы или буквв */
+        /* проверка на адекватность значения */
+        /* 1. проверка на то что введено число, а не символы или буквв */
         if (std::cin.fail())
         {
             std::cin.clear();                                                   // Очистка флага ошибки
@@ -423,6 +425,7 @@ void add_book_in_table(sqlite3 *db, const std::string &table_name)
             continue;
         }
 
+        /* 2. а вот теперь проверка на адекватность числа */
         if (ch == 1) // рекурсивно возвращаем функцию
         {
             return add_book_in_table(db, table_name); // рекурсия
