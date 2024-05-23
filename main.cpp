676441cea894dc75bd83b8e09dc1e8e1c287c97d
akrabatt <starwars-1999@yandex.ru>
2024-05-23 23:01:25 +0200

    end day

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ int main()
 	/* откроем базу данных */
 	sqlite3 *db = open_or_create_db(); // открываем базу данных
 	/* дадим выбор пользователю с какой таблицей работать либо создать новую */
-	chosen_table  = list_table(db);	//запишем результат в глобальную переменную
+	chosen_table = list_table(db); // запишем результат в глобальную переменную
 
-	std::cout << "its work: "<< chosen_table << std::endl;
+	std::cout << "its work: " << chosen_table << std::endl;
 	menu_ch = menu();
 	jump_to_choice(menu_ch, db, chosen_table);
 }
