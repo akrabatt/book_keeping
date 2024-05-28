d7b496a55ed03ada657df016f4641670a82eb78e
akrabatt <starwars-1999@yandex.ru>
2024-05-28 10:09:27 +0200

    int point

--- headders_src/str_book.h
+++ headders_src/str_book.h
@@ -1,12 +1,21 @@
 /* в этом файле мы создаем структуру книги */
 
 #include <string>
+#include <utility>
 
 typedef struct BOOK
 {
-    int id;             // пременная для идентификатора книги
-    std::string title;  // пременная для названия книги
-    std::string author; // пременная для автора книги
-    int year;           // пременная для года выпуска книги
-    std::string genre;  // пременная для жанра книги
+    std::pair<std::string, int> id;             // id пременная для идентификатора книги
+    std::pair<std::string, std::string> title;  // пременная для названия книги
+    std::pair<std::string, std::string> author; // пременная для автора книги
+    std::pair<std::string, int> year;           // пременная для года выпуска книги
+    std::pair<std::string, std::string> genre;  // пременная для жанра книги
 };
+// typedef struct BOOK
+// {
+//     int id;             // пременная для идентификатора книги
+//     std::string title;  // пременная для названия книги
+//     std::string author; // пременная для автора книги
+//     int year;           // пременная для года выпуска книги
+//     std::string genre;  // пременная для жанра книги
+// };
